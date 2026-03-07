a32378a71daa03b747fe612ee6090bd7b909b40b
rakafebriansy <rakafebrian19@gmail.com>
2026-03-07 21:13:22 +0700

    init restful api app

---
+++ src/application/database.js
@@ -0,0 +1,47 @@
+import {PrismaClient} from "@prisma/client";
+import {logger} from "./logging.js";
+import { PrismaMariaDb } from "@prisma/adapter-mariadb";
+
+const adapter = new PrismaMariaDb({
+  host: "localhost",
+  port: 3306,
+  connectionLimit: 5,
+});
+
+export const prismaClient = new PrismaClient({
+    adapter,
+    log: [
+        {
+            emit: 'event',
+            level: 'query',
+        },
+        {
+            emit: 'event',
+            level: 'error',
+        },
+        {
+            emit: 'event',
+            level: 'info',
+        },
+        {
+            emit: 'event',
+            level: 'warn',
+        },
+    ],
+});
+
+prismaClient.$on('error', (e) => {
+    logger.error(e);
+});
+
+prismaClient.$on('warn', (e) => {
+    logger.warn(e);
+});
+
+prismaClient.$on('info', (e) => {
+    logger.info(e);
+});
+
+prismaClient.$on('query', (e) => {
+    logger.info(e);
+});
